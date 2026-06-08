b93363fd8a48d6b5ca9dbbd38389aa2b224167fd
wblade-jg <juliangarzonalvarezwb9@gmail.com>
2026-06-08 14:40:33 -0500

    refactor: separar control plane y data plane en puertos independientes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use http_body_util::Full;
-use hyper::{Request, Response, body::Bytes, server::conn::http1, service::service_fn};
+use hyper::{Request, Response, StatusCode, body::Bytes, server::conn::http1, service::service_fn};
 use hyper_util::rt::TokioIo;
 use round_robin::RoundRobin;
 use server_group::ServerGroup;
@@ -17,14 +17,62 @@ mod server_group;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const PORT: u16 = 8080;
+    const CLIENTS_PORT: u16 = 8080;
+    let clients_socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), CLIENTS_PORT);
+    let clients_listener = TcpListener::bind(clients_socket).await?;
 
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), PORT);
-    let listener = TcpListener::bind(socket).await?;
+    println!("Escuchando clientes en el puerto: {CLIENTS_PORT}");
 
-    println!("Escuchando en el puerto {PORT}");
     let servers = Arc::new(get_servers());
 
+    tokio::spawn(async move { start_server_registry().await.unwrap() });
+
+    start_client_proxy_server(clients_listener, servers).await?;
+
+    Ok(())
+}
+
+async fn start_server_registry() -> Result<(), Box<dyn std::error::Error>> {
+    const REGISTRY_PORT: u16 = 8500;
+    let registry_socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), REGISTRY_PORT);
+    let registry_listener = TcpListener::bind(registry_socket).await?;
+
+    println!("Server registry escuchando en el puerto: {REGISTRY_PORT}");
+
+    loop {
+        let (stream, server_address) = registry_listener.accept().await?;
+        let io_stream = TokioIo::new(stream);
+        tokio::spawn(async move {
+            if let Err(_) = http1::Builder::new()
+                .serve_connection(io_stream, service_fn(|req| handle_registry(req, server_address)))
+                .await
+            {
+                println!("Error sirviendo conexion en el service registry");
+            }
+        });
+    }
+}
+
+async fn handle_registry(
+    req: Request<hyper::body::Incoming>,
+    address: SocketAddr
+) -> Result<Response<Full<Bytes>>, Infallible> {
+    println!("{:?}", address);
+    if req.uri().path() == "/register"{
+        println!("{address}");
+    }
+    let response = Response::builder()
+        .status(StatusCode::NO_CONTENT)
+        .body(Full::new(Bytes::new()))
+        .unwrap();
+
+    Ok(response)
+}
+
+async fn start_client_proxy_server(
+    listener: TcpListener,
+    servers: Arc<HashMap<String, ServerGroup>>,
+) -> Result<(), Box<dyn std::error::Error>> {
     loop {
         let (stream, _) = listener.accept().await?;
         let io_stream = TokioIo::new(stream);
