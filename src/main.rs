062bf64e290f01775e348a9c6f98e4f88e44f56e
wblade-jg <juliangarzonalvarezwb9@gmail.com>
2026-06-09 12:37:01 -0500

    refactor: encapsular plano de control en estructura ServiceRegistry

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use http_body_util::{BodyExt, Full};
-use hyper::body::Buf;
-use hyper::{Request, Response, StatusCode, body::Bytes, server::conn::http1, service::service_fn};
+use http_body_util::Full;
+use hyper::{Request, Response, body::Bytes, server::conn::http1, service::service_fn};
 use hyper_util::rt::TokioIo;
-use serde::Deserialize;
 use services::Service;
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -12,6 +10,8 @@ use std::{
 };
 use tokio::net::TcpListener;
 use tokio::sync::RwLock;
+
+use crate::services::ServiceRegistry;
 
 mod load_balancing;
 mod round_robin;
@@ -25,102 +25,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Escuchando clientes en el puerto: {PORT}");
 
-    let services_map = Arc::new(RwLock::new(get_available_services()));
-    let _services_map = Arc::clone(&services_map);
+    let service_registry = ServiceRegistry::new(8500);
+    let services_map = service_registry.get_available_services();
 
-    tokio::spawn(async move {
-        start_service_registry(Arc::clone(&_services_map))
-            .await
-            .unwrap()
-    });
+    tokio::spawn(async move { service_registry.start_service_registry().await.unwrap() });
 
     start_client_proxy_server(listener, &services_map).await?;
 
     Ok(())
-}
-
-async fn start_service_registry(
-    services_map: Arc<RwLock<HashMap<String, Service>>>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    const PORT: u16 = 8500;
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), PORT);
-    let listener = TcpListener::bind(socket).await?;
-
-    println!("Service registry escuchando en el puerto: {PORT}");
-
-    loop {
-        let (stream, server_address) = listener.accept().await?;
-        let io_stream = TokioIo::new(stream);
-        let services = Arc::clone(&services_map);
-
-        tokio::spawn(async move {
-            if let Err(_) = http1::Builder::new()
-                .serve_connection(
-                    io_stream,
-                    service_fn(|req| {
-                        handle_registry(req, server_address.ip(), Arc::clone(&services))
-                    }),
-                )
-                .await
-            {
-                println!("Error sirviendo conexion en el service registry");
-            }
-        });
-    }
-}
-
-#[derive(Deserialize)]
-struct RegisterServiceInfo {
-    route_prefix: String,
-    port: u16,
-}
-
-async fn handle_registry(
-    req: Request<hyper::body::Incoming>,
-    ip_address: IpAddr,
-    services: Arc<RwLock<HashMap<String, Service>>>,
-) -> Result<Response<Full<Bytes>>, Infallible> {
-    println!("{:?}", ip_address);
-    let mut response = Response::builder().body(Full::new(Bytes::new())).unwrap();
-
-    if req.uri().path() == "/register" {
-        println!("{ip_address}");
-        if let Some(payload) = get_payload(req.into_body()).await {
-            let mut _services = services.write().await;
-            let new_server_address = format!("{}:{}", ip_address, payload.port);
-
-            if let Some(new_service) = _services.get_mut(&payload.route_prefix) {
-                new_service.add_instance_server(String::from(new_server_address));
-                println!("Servicio actualizado: {}", &payload.route_prefix);
-            } else {
-                let mut new_service = Service::new();
-                new_service.add_instance_server(String::from(new_server_address));
-                _services.insert(String::from(&payload.route_prefix), new_service);
-                println!("Servicio registrado: {}", &payload.route_prefix);
-            }
-            *response.status_mut() = StatusCode::ACCEPTED;
-        } else {
-            println!("Error en la solicitud");
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        }
-    } else {
-        *response.status_mut() = StatusCode::NOT_FOUND;
-    }
-
-    Ok(response)
-}
-
-async fn get_payload(req_body: hyper::body::Incoming) -> Option<RegisterServiceInfo> {
-    if let Ok(collected) = req_body.collect().await {
-        let reader = collected.aggregate().reader();
-        if let Ok(payload) = serde_json::from_reader::<_, RegisterServiceInfo>(reader) {
-            Some(payload)
-        } else {
-            None
-        }
-    } else {
-        None
-    }
 }
 
 async fn start_client_proxy_server(
@@ -161,17 +73,4 @@ async fn handle_request(
     }
 
     Ok(Response::new(Full::from(Bytes::from(res_body))))
-}
-
-fn get_available_services() -> HashMap<String, Service> {
-    let mut services_map: HashMap<String, Service> = HashMap::new();
-    let mut new_service = Service::new();
-
-    new_service.add_instance_server(String::from("192.168.100.10"));
-    new_service.add_instance_server(String::from("192.168.100.20"));
-    new_service.add_instance_server(String::from("192.168.100.30"));
-    new_service.add_instance_server(String::from("192.168.100.40"));
-
-    services_map.insert(String::from("/users"), new_service);
-    services_map
 }
--- src/services.rs
+++ src/services.rs
@@ -1,9 +1,33 @@
 use crate::{load_balancing::LoadBalancingStrategy, round_robin::RoundRobin};
+use http_body_util::{BodyExt, Full};
+use hyper::body::Buf;
+use hyper::{Request, Response, StatusCode, body::Bytes, server::conn::http1, service::service_fn};
+use hyper_util::rt::TokioIo;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::sync::Arc;
 use std::sync::Mutex;
+use std::{
+    convert::Infallible,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+};
+use tokio::net::TcpListener;
+use tokio::sync::RwLock;
 
 pub struct Service {
     servers_list: Vec<String>,
     load_balancer: Mutex<Box<dyn LoadBalancingStrategy + Send>>,
+}
+
+#[derive(Deserialize)]
+struct RegisterServiceInfo {
+    route_prefix: String,
+    port: u16,
+}
+
+pub struct ServiceRegistry {
+    port: u16,
+    services_map: Arc<RwLock<HashMap<String, Service>>>,
 }
 
 impl Service {
@@ -23,5 +47,109 @@ impl Service {
         balancer.num_servers(self.servers_list.len());
         let index = balancer.get_next_index();
         String::from(&self.servers_list[index])
+    }
+}
+
+fn get_available_services() -> HashMap<String, Service> {
+    let mut services_map: HashMap<String, Service> = HashMap::new();
+    let mut new_service = Service::new();
+
+    new_service.add_instance_server(String::from("192.168.100.10"));
+    new_service.add_instance_server(String::from("192.168.100.20"));
+    new_service.add_instance_server(String::from("192.168.100.30"));
+    new_service.add_instance_server(String::from("192.168.100.40"));
+
+    services_map.insert(String::from("/users"), new_service);
+    services_map
+}
+
+impl ServiceRegistry {
+    pub fn new(port: u16) -> Self {
+        Self {
+            port,
+            services_map: Arc::new(RwLock::new(get_available_services())),
+        }
+    }
+
+    pub fn get_available_services(&self) -> Arc<RwLock<HashMap<String, Service>>> {
+        Arc::clone(&self.services_map)
+    }
+
+    pub async fn start_service_registry(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), self.port);
+        let listener = TcpListener::bind(socket).await?;
+
+        println!("Service registry escuchando en el puerto: {}", self.port);
+        
+        let registry_map = Arc::clone(&self.services_map);
+
+        loop {
+            let (stream, server_address) = listener.accept().await?;
+            let io_stream = TokioIo::new(stream);
+            let connection_map = Arc::clone(&registry_map);
+
+            tokio::spawn(async move {
+                if let Err(_) = http1::Builder::new()
+                    .serve_connection(
+                        io_stream,
+                        service_fn(|req| {
+                            Self::handle_registry(req, server_address.ip(), Arc::clone(&connection_map))
+                        }),
+                    )
+                    .await
+                {
+                    println!("Error sirviendo conexion en el service registry");
+                }
+            });
+        }
+    }
+
+    async fn handle_registry(
+        req: Request<hyper::body::Incoming>,
+        ip_address: IpAddr,
+        services: Arc<RwLock<HashMap<String, Service>>>,
+    ) -> Result<Response<Full<Bytes>>, Infallible> {
+        println!("{:?}", ip_address);
+        let mut response = Response::builder().body(Full::new(Bytes::new())).unwrap();
+
+        if req.uri().path() == "/register" {
+            println!("{ip_address}");
+            if let Some(payload) = get_payload(req.into_body()).await {
+                let mut _services = services.write().await;
+                let new_server_address = format!("{}:{}", ip_address, payload.port);
+
+                if let Some(new_service) = _services.get_mut(&payload.route_prefix) {
+                    new_service.add_instance_server(String::from(new_server_address));
+                    println!("Servicio actualizado: {}", &payload.route_prefix);
+                } else {
+                    let mut new_service = Service::new();
+                    new_service.add_instance_server(String::from(new_server_address));
+                    _services.insert(String::from(&payload.route_prefix), new_service);
+                    println!("Servicio registrado: {}", &payload.route_prefix);
+                }
+                *response.status_mut() = StatusCode::ACCEPTED;
+            } else {
+                println!("Error en la solicitud");
+                *response.status_mut() = StatusCode::NOT_FOUND;
+            }
+        } else {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+
+        Ok(response)
+    }
+}
+
+async fn get_payload(req_body: hyper::body::Incoming) -> Option<RegisterServiceInfo> {
+    if let Ok(collected) = req_body.collect().await {
+        let reader = collected.aggregate().reader();
+
+        if let Ok(payload) = serde_json::from_reader::<_, RegisterServiceInfo>(reader) {
+            Some(payload)
+        } else {
+            None
+        }
+    } else {
+        None
     }
 }
